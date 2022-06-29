e76f6b8a62c6d189bbed4f1666fb8d09ad5a0204
jose izaias alcantara lima <joseizaias@gmail.com>
2022-06-29 09:52:08 -0300

    NodeJS - Desafio 02 - Middlewares - concluido!

--- src/index.js
+++ src/index.js
@@ -10,19 +10,78 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find( user => user.username === username );
+
+  if ( !user ) {
+    return response.status(404).json({
+      error: `The username: ${ username } does not exist!`
+    });
+  }
+
+  request.user = user;
+  next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ( !user.pro && user.todos.length === 10 ) {
+    return response.status(403).json({
+      error: `The username: ${ user.username } has a free plan and ten todos`
+    });
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find( user => user.username === username );
+
+  if ( !user ) {
+    return response.status(404).json({
+      error: `The username: ${ username } does not exist!`
+    });
+  }
+
+  if ( !validate( id) ) {
+    return response.status(400).json({
+      error: `This ID: ${ id } is not a valid UUID.`
+    })
+  }
+  const todo = user.todos.find( todo => todo.id === id );
+
+  if ( !todo ) {
+    return response.status(404).json({
+      error: `The todo ID: ${ id } does not exist!`
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find( user => user.id === id );
+
+  if ( !user ) {
+    return response.status(404).json({
+      error: `The user with the ID: ${ id } does not exist!`
+    });
+  }
+
+  request.user = user;
+  next();
+
+
 }
 
 app.post('/users', (request, response) => {
